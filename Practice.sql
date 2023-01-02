fe075ba28777261883eaa0afcec8608a61db0c8f
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-01-02 17:56:06 -0500

    Update Practice.sql

--- Practice.sql
+++ Practice.sql
@@ -1,106 +1,108 @@
 CREATE TABLE school (
-scID VARCHAR (32),
-name VARCHAR (64) default '',
-city VARCHAR (16) default '',
-primary key (scID));
+scID varchar (32),
+name varchar (64) DEFAULT '',
+city varchar (16) DEFAULT '',
+PRIMARY KEY (scID));
 
 CREATE TABLE wizard (
-magID VARCHAR (16),
-wizName VARCHAR (48) default '',
-dateBorn date default sysdate, 
-primary key (magID));
+magID varchar (16),
+wizName varchar (48) default '',
+dateBorn date DEFAULT sysdate, 
+PRIMARY KEY (magID));
 
 CREATE TABLE attend (
-who VARCHAR (16),
-what VARCHAR (32) not null,
-when char (4) not null,
-primary key (who),
-foreign key (who) references wizard,
-foreign key (what) references school);
+who varchar (16),
+what varchar (32) NOT NULL,
+when char (4) NOT NULL,
+PRIMARY KEY (who),
+FOREIGN KEY (who) REFERENCES wizard,
+FOREIGN KEY (what) REFERENCES school);
 
 CREATE TABLE competition ( 
-compID VARCHAR (28),
-year char (4) not null,
-host VARCHAR (32) not null, 
-primary key (compID),
-foreign key (host) references school);
+compID varchar (28),
+year char (4) NOT NULL,
+host varchar (32) NOT NULL, 
+PRIMARY KEY (compID),
+FOREIGN KEY (host) REFERENCES school);
 
 CREATE TABLE permit (
-eventID VARCHAR (28), 
+eventID varchar (28), 
 permID varchar (20),
-player varchar (16) not null,
-primary key (eventID, permID),
-foreign key (eventID) references competition, 
-foreign key (player) references wizard);
+player varchar (16) NOT NULL,
+PRIMARY KEY (eventID, permID),
+FOREIGN KEY (eventID) REFERENCES competition, 
+FOREIGN KEY (player) REFERENCES wizard);
 
 CREATE TABLE representation ( 
 schoolID varchar (32),
 gameID varchar (28), 
-permitID varchar (20), primary key (schoolID, gameID),
-foreign key (schoolID) references school,
-foreign key (gameID, permitID) references permit);
+permitID varchar (20), 
+PRIMARY KEY (schoolID, gameID),
+FOREIGN KEY (schoolID) REFERENCES school,
+FOREIGN KEY (gameID, permitID) REFERENCES permit);
 
 CREATE TABLE standing (
 gameID varchar (28),
 permitID varchar (20),
-position number (*,O) not null check (position=> 1),
-primary key (gameID, permitID),
-foreign key (gameID, permitID) references permit);
+position number (*,O) NOT NULL CHECK (position=> 1),
+PRIMARY KEY (gameID, permitID),
+FOREIGN KEY (gameID, permitID) REFERENCES permit);
 
 CREATE TABLE problem (
 pID varchar (32),
-title varchar (96) not null,
-text varchar (1860) not null,
-author varchar (16) not null,
-difficultyLevel number (*,0) check (1 <= difficultyLevel and difficultyLevel =< 100),
-primary key (pID),
-foreign key (author) references wizard);
+title varchar (96) NOT null,
+text varchar (1860) NOT null,
+author varchar (16) NOT null,
+difficultyLevel number (*,0) CHECK (1 <= difficultyLevel AND difficultyLevel =< 100),
+PRIMARY KEY (pID),
+FOREIGN KEY (author) REFERENCES wizard);
 
 CREATE TABLE selectedProblem (
 problemID varchar (32), 
-competitionID varchar (28) not null,
-selector varchar (16) not null, 
-testData varchar (2048) default '',
-gradingAdvice varchar (1972) default '', 
-primary key (problemID, competitionID),
-foreign key (problemID) references problem,
-foreign key (competitionID) references competition,
-foreign key (selector) references wizard);
+competitionID varchar (28) NOT null,
+selector varchar (16) NOT null, 
+testData varchar (2048) DEFAULT '',
+gradingAdvice varchar (1972) DEFAULT '', 
+PRIMARY KEY (problemID, competitionID),
+FOREIGN KEY (problemID) REFERENCES problem,
+FOREIGN KEY (competitionID) REFERENCES competition,
+FOREIGN KEY (selector) REFERENCES wizard);
 
 CREATE TABLE grader (
-graderId varchar(16), 
+graderId varchar (16), 
 firstRec varchar (16), 
 secondRec varchar (16),
-primary key (graderId),
-foreign key (graderID) references wizard,
-foreign key (firstRec) references wizard,
-foreign key (secondRec) references wizard,
-constraint notSelf check (graderID != firstRec and graderID != secondRec), 
-constraint twoRecs check (firstRec != secondRec));
+PRIMARY KEY (graderId),
+FOREIGN KEY (graderID) REFERENCES wizard,
+FOREIGN KEY (firstRec) REFERENCES wizard,
+FOREIGN KEY (secondRec) REFERENCES wizard,
+CONSTRAINT notSelf CHECK (graderID != firstRec AND graderID != secondRec), 
+CONSTRAINT twoRecs CHECK (firstRec != secondRec));
 
 CREATE TABLE approvedGrader (
 grID varchar (16),
 prID varchar (32),
 cpID varchar (28), 
-primary key (grID, prID, cpID), 
-foreign key (grID) references grader, 
-foreign key (prID, cpID) references selectedProblem);
+PRIMARY KEY (grID, prID, cpID), 
+FOREIGN KEY (grID) REFERENCES grader, 
+FOREIGN KEY (prID, cpID) REFERENCES selectedProblem);
 
 CREATE TABLE gradedSolution (
 eventID varchar (28),
 permitID varchar (20),
 problemID varchar (32),
 graderID varchar (16),
-grade number (*, 0) check (0 <= grade and grade <= 100), 
+grade number (*, 0) check (0 <= grade AND grade <= 100), 
 PRIMARY KEY (eventID, permitID, problemID),
-FOREIGN KEY (eventID, permitID) references permit,
-FOREIGN KEY (problemID, eventID) references selectedProblem,
-FOREIGN KEY (graderID, problemID, eventID) references approvedGrader);
+FOREIGN KEY (eventID, permitID) REFERENCES permit,
+FOREIGN KEY (problemID, eventID) REFERENCES selectedProblem,
+FOREIGN KEY (graderID, problemID, eventID) REFERENCES approvedGrader);
 
 CREATE TABLE graderPay (
 problemID varchar (32),
-unitPay number (*,0) check (unitPay => 0), primary key (problemID),
-foreign key (problemID) references problem);
+unitPay number (*,0) CHECK (unitPay => 0), 
+PRIMARY KEY (problemID),
+FOREIGN KEY (problemID) REFERENCES problem);
 
 /* Find problem ID, title, and author's name of those problems that have never been selected for a competition */
 
