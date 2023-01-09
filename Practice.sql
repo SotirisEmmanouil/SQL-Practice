31c3076a0b39b292b082b0b88bfd6362b58bbb62
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-01-09 17:37:10 -0500

    Update Practice.sql

--- Practice.sql
+++ Practice.sql
@@ -156,7 +156,7 @@ WHERE selector IN (SELECT magID FROM wizard WHERE wizName = 'Antonin Dolohov';
   s2.competitionID = c2.compID AND
   c.year != c2.year;               
 
- /* A new school has been created with school ID = 3498558 ,named = School1 , and located in city New York */
+ /* A new school has been created with school ID = 3498558 ,name = School1 , and located in city New York */
    
  INSERT INTO school (scID, name, city) VALUES 
  (3498558, School1, New York);             
